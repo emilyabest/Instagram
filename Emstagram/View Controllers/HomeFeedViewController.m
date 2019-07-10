1b412490db93238080a91834625bcac14309176e
Emily Best <emilyabest@fb.com>
2019-07-09 18:01:34 -0700

    Working on Step 5. Camera feature is working. I've brought over CodePath's code for making a post. Next step is to connect the selected image to the post (also make the Post VC and also connect the user's inputed text as the caption.)

--- Emstagram/View Controllers/HomeFeedViewController.m
+++ Emstagram/View Controllers/HomeFeedViewController.m
@@ -6,10 +6,15 @@
 //  Copyright © 2019 emilyabest. All rights reserved.
 //
 
+//NSLog(@"☀️ Checkpoint 1");
+
+
 #import "HomeFeedViewController.h"
 #import <Parse/Parse.h>
+#import <UIKit/UIKit.h>
 
-@interface HomeFeedViewController ()
+@interface HomeFeedViewController () <UIImagePickerControllerDelegate, UINavigationControllerDelegate>
+@property (strong, nonatomic) UIImage *chosenImage;
 
 @end
 
@@ -17,7 +22,6 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
 }
 
 /**
@@ -29,6 +33,44 @@
     }];
     
     [self performSegueWithIdentifier:@"loginSegue" sender:nil];
+}
+
+/**
+ User tapped the camera button
+ */
+- (IBAction)didTapCamera:(id)sender {
+    // Instantiate a UIImagePickerController
+    UIImagePickerController *imagePickerVC = [UIImagePickerController new];
+    imagePickerVC.delegate = self;
+    imagePickerVC.allowsEditing = YES;
+    
+    // Check if camera is supported
+    if ([UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypeCamera]) {
+        imagePickerVC.sourceType = UIImagePickerControllerSourceTypeCamera;
+    } else {
+        NSLog(@"Camera 🚫 available so we will use photo library instead");
+        imagePickerVC.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
+    }
+    
+    // Present UIImagePickerController
+    [self presentViewController:imagePickerVC animated:YES completion:nil];
+}
+
+/**
+ The delegate method
+ */
+- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo: (NSDictionary<NSString *,id> *)info {
+    
+    // Get the image captured by the UIImagePickerController
+    UIImage *originalImage = info[UIImagePickerControllerOriginalImage];
+    UIImage *editedImage = info[UIImagePickerControllerEditedImage];
+    
+    // Do something with the images (based on your use case)
+    self.chosenImage = editedImage;
+    
+    // Dismiss UIImagePickerController to go back to your original view controller
+    [self dismissViewControllerAnimated:YES completion:nil];
+//    [self performSegueWithIdentifier:@"firstSegue" sender:nil];
 }
 
 /*
